97ddff7a50829a1167221e4edbfe601960d1e59b
Martin Midolesov (c) <mmidolesov@vmware.com>
2019-01-12 13:58:35 +0200

    Added Message reousrce class and application.properties

--- config-client/src/main/java/com/techprimers/config/configclient/MessageResource.java
+++ config-client/src/main/java/com/techprimers/config/configclient/MessageResource.java
@@ -1,15 +1,17 @@
 package com.techprimers.config.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("/rest")
 @RestController
 public class MessageResource {
 
-	@Value("message")
+	@Value("${message: Default message}")
 	private String message;
 	
 	@GetMapping("/message")
